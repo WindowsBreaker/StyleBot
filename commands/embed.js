1970bb84ee35412f15eef7e035a1f7d6a0c537b6
Lua <93476072+WindowsBreaker@users.noreply.github.com>
2021-12-19 11:46:17 +0100

    Create embed.js

---
+++ commands/embed.js
@@ -0,0 +1,30 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageEmbed } = require("discord.js");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("embed")
+    .setDescription("create a new embed Object")
+    .addStringOption(option =>
+      option
+        .setName("title")
+	.setDescription("title of the embed")
+	.setRequired(true))
+    .addStringOption(option =>
+      option
+        .setName("description")
+	.setDescription("description of the embed")
+	.setRequired(true))
+    .addStringOption(option =>
+      option
+        .setName("footer")
+	.setDescription("footer of the embed")
+	.setRequired(true)),
+  async execute(interaction) {
+      const embed = new MessageEmbed()
+        .setTitle(interaction.options.getString('title'))
+    .setDescription(interaction.options.getString('description'))
+    .setFooter(interaction.options.getString('footer'))
+  interaction.reply({embeds: [embed]});
+  },
+};

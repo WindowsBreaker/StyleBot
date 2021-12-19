d7f3796b7ada72c057c8fb561ffdd606a1263dc3
Lua <93476072+WindowsBreaker@users.noreply.github.com>
2021-12-19 11:42:06 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,64 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
+
+const fs = require('fs');
+const { Client, Collection, Intents } = require('discord.js');
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+const join = (string => process.env[string]);
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+client.on("ready", () => {
+  client.user.setActivity("/help")
+})
+client.commands = new Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	client.commands.set(command.data.name, command);
+}
+
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+client.on('interactionCreate', async (interaction) => {
+	if (!interaction.isCommand()) return;
+
+	const command = client.commands.get(interaction.commandName);
+
+	if (!command) return;
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		return interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+	}
+});
+
+const commands = [];
+
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	commands.push(command.data.toJSON());
+}
+
+const rest = new REST({ version: '9' }).setToken(join("token"));
+
+rest.put(Routes.applicationCommands(join("id")), { body: commands })
+	.then(() => console.log("(/) Logged"))
+	.catch(console.error);
+
+client.login(join("token"));

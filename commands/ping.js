4533a745b0b6086388dbbcd429601b8027ab6085
Lua <93476072+WindowsBreaker@users.noreply.github.com>
2021-12-23 11:03:34 +0100

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -6,9 +6,17 @@ module.exports = {
     .setName('ping')
     .setDescription ('Pong!'),
   async execute(interaction) {
+    let totalSeconds = (message.client.uptime / 1000);
+    let days = Math.floor(totalSeconds / 86400);
+    totalSeconds %= 86400;
+    let hours = Math.floor(totalSeconds / 3600);
+    totalSeconds %= 3600;
+    let minutes = Math.floor(totalSeconds / 60);
+    let seconds = Math.floor(totalSeconds % 60);
+    const uptime = `${days}gg, ${hours}h, ${minutes}m, ${seconds}s`
     await interaction.reply({embeds: [
       new MessageEmbed()
-        .setTitle(`<:styleSuccess:920305910335823882> ${interaction.client.ws.ping}ms`)
+        .setTitle(`<:styleSuccess:920305910335823882> Online\n\nAPI Latency: ${interaction.client.ws.ping}ms\nLatency: ${Date.now() - interaction.createdTimestamp}ms\nUptime: ${uptime}`)
         .setColor("#3da777")
       ],
     });
